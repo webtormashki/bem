7c1cf3931177d4e8b813610e3a825431185d785e
Tanya Yubko <webtormashki@gmail.com>
2020-05-15 00:49:02 +0300

    add promo and reasons

--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -1,2 +1,9 @@
 import "%modules%/header/header";
+import "%modules%/subheader/subheader";
+import "%modules%/promo/promo";
+import "%modules%/reasons/reasons";
+import "%modules%/mobile/mobile";
+import "%modules%/choice/choice";
+import "%modules%/require/require";
+import "%modules%/world/world";
 import "%modules%/footer/footer"
