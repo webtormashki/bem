bfe1ba852289a3038e30ff017b1c8acb406a26ee
Tanya Yubko <webtormashki@gmail.com>
2020-05-17 15:32:47 +0300

    add smooth scroll to nav

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -1,17 +1,57 @@
+import jQuery from "jquery";
+	// Smooth scroll and pageup
+
+	
+	$(function(){
+	$("a[href^='#up']").click(function(){
+			var _href = $(this).attr("href");
+			$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+			return false;
+	});
+	});
+	$(function(){
+	$("a[href^='#reasons']").click(function(){
+			var _href = $(this).attr("href");
+			$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+			return false;
+	});
+	});
+	$(function(){
+	$("a[href^='#mobile']").click(function(){
+			var _href = $(this).attr("href");
+			$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+			return false;
+	});
+	});
+	$(function(){
+	$("a[href^='#choice']").click(function(){
+			var _href = $(this).attr("href");
+			$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+			return false;
+	});
+	});
+	$(function(){
+	$("a[href^='#world']").click(function(){
+			var _href = $(this).attr("href");
+			$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+			return false;
+	});
+	});
+
 window.addEventListener('DOMContentLoaded', () => {
-    const menu = document.querySelector('.menu'),
-    menuItem = document.querySelectorAll('.menu__item'),
-    hamburger = document.querySelector('.hamburger');
+	const menu = document.querySelector('.menu'),
+	menuItem = document.querySelectorAll('.menu__item'),
+	hamburger = document.querySelector('.hamburger');
 
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('hamburger_active');
-        menu.classList.toggle('menu_active');
-    });
+	hamburger.addEventListener('click', () => {
+		hamburger.classList.toggle('hamburger_active');
+		menu.classList.toggle('menu_active');
+	});
 
-    menuItem.forEach(item => {
-        item.addEventListener('click', () => {
-            hamburger.classList.toggle('hamburger_active');
-            menu.classList.toggle('menu_active');
-        })
-    })
+	menuItem.forEach(item => {
+		item.addEventListener('click', () => {
+			hamburger.classList.toggle('hamburger_active');
+			menu.classList.toggle('menu_active');
+		})
+	})
 })
